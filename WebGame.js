d4fe595a9afdcafba40ddce22640ef329301f753
lukool1800 <156939007+lukool1800@users.noreply.github.com>
2024-10-07 11:55:21 -0700

    Update WebGame.js

--- WebGame.js
+++ WebGame.js
@@ -18,12 +18,14 @@ function removeItemOnce(arr, value) {
 console.log(cardSlots.slice(0, 4));
 console.log(cardSlots.slice(4, 6));
 console.log(cardSlots.slice(6, 10));
-var grid = [[1,2,3,4],[11,5,6,12],[7,8,9,10]];
+var grid = [1,2,3,4,11,5,6,12,7,8,9,10];
 for (let i=0; i<4; i++){
     for(let a=0; a<3; a++){
         var spotNow = grid[i];
+        //boxes
         var box = new Rectangle(getWidth()/4 - 10, getHeight()/3 - 10);
         box.setPosition(getWidth()/4*i,getHeight()/3*a);
+        box.setColor("blue");
         if(a==1){
             if(i==0){
                 box.setColor("white");
@@ -32,5 +34,26 @@ for (let i=0; i<4; i++){
             }
         }
         add(box);
+        //texts
+        var text = new Text("temp", "30pt Arial");
+        text.setText(cardSlots[spotNow-1]);
+        text.setPosition(getWidth()/4*i,getHeight()/3*a+100);
+        if(a==1){
+            if(i==0){
+                text.setColor("white");
+            }else if(i==3){
+                text.setColor("white");
+            }
+        }
+        add(text);
+    }
+}
+mouseMoveMethod(turnRed);
+function turnRed(e){
+    var elem = getElementAt(e.getX(), e.getY());
+    var mouseX = e.getX();
+    var mouseY = e.getY();
+    if (elem != null) {
+        elem.setColor("red");
     }
 }
