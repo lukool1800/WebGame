c1d22d5eeb1c5e3ed4d43c85649af623855744ce
Builder-roberts <99707221+Builder-roberts@users.noreply.github.com>
2024-10-08 12:32:48 -0700

    Update WebGame.js
    added Card Class

--- WebGame.js
+++ WebGame.js
@@ -4,6 +4,50 @@ const canvas = document.querySelector(".gameCanvas");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
+//Card Class
+//Card Class
+
+class Card{
+    constructor(fronturl, backurl, cardtype) {
+        //declare variables
+        this.front = fronturl;
+        this.back = backurl;
+        this.flipped = true;
+        //true = back of card
+        this.backimage = new WebImage(this.back);
+        add(this.backimage);
+        this.frontimage = new WebImage(this.front);
+        this.cardtype = cardtype;
+    }
+    //functions (setsize, setposition, flip, change front?, swap card?)
+    setSize(x,y){
+        this.backimage.setSize(x,y);
+        this.frontimage.setSize(x,y);
+    }
+    
+    setPosition(x,y){
+        this.backimage.setPosition(x,y);
+        this.frontimage.setPosition(x,y);
+    }
+    
+    flip(){
+        if(this.flipped){
+            this.flipped = false;
+            remove(this.backimage);
+            add(this.frontimage);
+        } else {
+            this.flipped = true;
+            remove(this.frontimage);
+            add(this.backimage);
+        }
+    }
+}
+//example
+/*let testcard = new Card("https://static.codehs.com/img/library/characters/leopard.jpg","https://static.codehs.com/img/library/characters/chameleon.jpg", "testcard");
+testcard.setPosition(0,0);
+testcard.setSize(100,200);
+*/
+
 
 //Shuffle fuction
 var cardSlots = [1,2,3,4,5,6,7,8,9,10];
