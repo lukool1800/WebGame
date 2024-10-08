0ba1782e5c606992f95ca6d0e4829182c8914a74
lukool1800 <156939007+lukool1800@users.noreply.github.com>
2024-10-08 10:13:44 -0700

    Update WebGame.js

--- WebGame.js
+++ WebGame.js
@@ -1,3 +1,4 @@
+// Shuffle
 var cardSlots = [1,2,3,4,5,6,7,8,9,10];
 var cardTypeList = ["death", "coin", "stop", "draw", "swap", "coin", "stop", "draw", "swap", "coin"];
 var cardType = 0;
@@ -19,6 +20,7 @@ console.log(cardSlots.slice(0, 4));
 console.log(cardSlots.slice(4, 6));
 console.log(cardSlots.slice(6, 10));
 var grid = [1,2,3,4,11,5,6,12,7,8,9,10];
+//Add boxes and text
 for (let i=0; i<4; i++){
     for(let a=0; a<3; a++){
         var spotNow = grid[i];
@@ -26,34 +28,38 @@ for (let i=0; i<4; i++){
         var box = new Rectangle(getWidth()/4 - 10, getHeight()/3 - 10);
         box.setPosition(getWidth()/4*i,getHeight()/3*a);
         box.setColor("blue");
-        if(a==1){
-            if(i==0){
-                box.setColor("white");
-            }else if(i==3){
-                box.setColor("white");
-            }
-        }
         add(box);
-        //texts
-        var text = new Text("temp", "30pt Arial");
-        text.setText(cardSlots[spotNow-1]);
-        text.setPosition(getWidth()/4*i,getHeight()/3*a+100);
         if(a==1){
             if(i==0){
-                text.setColor("white");
+                remove(box);
             }else if(i==3){
-                text.setColor("white");
+                remove(box);
             }
         }
-        add(text);
+        //texts
+        var cardText = new Text("temp", "30pt Arial");
+        cardText.setText(cardSlots[spotNow-1]);
+        cardText.setPosition(getWidth()/4*i,getHeight()/3*a+100);
+        add(cardText);
+        if(a==1){
+            if(i==0){
+                remove(cardText);
+            }else if(i==3){
+                remove(cardText);
+            }
+        }
+        
     }
 }
+// Makes the element where the mouse is turn red (can't undo yet)
 mouseMoveMethod(turnRed);
 function turnRed(e){
     var elem = getElementAt(e.getX(), e.getY());
     var mouseX = e.getX();
     var mouseY = e.getY();
     if (elem != null) {
-        elem.setColor("red");
+        if (elem.constructor == Rectangle){
+            elem.setColor("red");
+        }
     }
 }
